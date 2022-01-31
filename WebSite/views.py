a49ec68396f9725e9071928fba40dc35210e3764
eduardo-trejo-es <alfredo.trejo.es@outlook.com>
2022-01-30 19:24:06 -0600

    making works

--- WebSite/views.py
+++ WebSite/views.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, request, flash
-from StockPricePredictionFIles.ForcastingGeneScript import PriceForcaster
 from flask import Flask, render_template
 from flask import Flask, redirect, url_for, render_template, session
 from flask_wtf import FlaskForm
