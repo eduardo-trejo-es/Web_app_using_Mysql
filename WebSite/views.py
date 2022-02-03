28f8d8c3906e9df2d1780536c889dee810bb3375
eduardo-trejo-es <alfredo.trejo.es@outlook.com>
2022-02-02 22:32:22 -0600

    implementing the post mothod

--- WebSite/views.py
+++ WebSite/views.py
@@ -10,7 +10,30 @@ from wtforms import validators, SubmitField
 views = Blueprint('views', __name__)
 
 
-@views.route('/', methods=['POST', 'GET'])
-def home():
-    return render_template("home.html")
+@views.route('/save_email', methods=["POST", "GET"])
+def Save_email():
+    if request.method == "POST":
+        email = request.form["email"]
+        return redirect(url_for("views.write_email_func", email_send_val=email, other_A="hello world"))
+    else:
+        return render_template("Save_email.html")
+
+@views.route("/write_email/<email_send_val>/<other_A>")
+def write_email_func(email_send_val,other_A):
+    return f"<h1>{email_send_val,other_A}</h1>"
+
+
+
+"""@views.route('/save_email', methods=['POST', 'GET'])
+def Save_email():
+    if request.method == "POST":
+        email = request.form["email"]
+        print(email)
+        return redirect(url_for("test_name_func", email_test=email))
+    else:
+        return render_template("Save_email.html")
+
+@views.route("/<email_url>")
+def test_name_func(email_url):
+    return f"<h1>{email_url}</h1>"""
 
